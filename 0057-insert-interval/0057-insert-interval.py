160e0361ef84a44ba66dd98c95a5a496dfdd08c3
Shivani Sawargave <shivanisawargave21@gmail.com>
2026-08-23 22:04:08 +0530

    Time: 0 ms (100%), Space: 21.2 MB (89.28%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.py
@@ -0,0 +1,23 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        result = []
+
+        for interval in intervals:
+
+            # Before new interval
+            if interval[1] < newInterval[0]:
+                result.append(interval)
+
+            # After new interval
+            elif interval[0] > newInterval[1]:
+                result.append(newInterval)
+                newInterval = interval
+
+            # Overlapping
+            else:
+                newInterval[0] = min(newInterval[0], interval[0])
+                newInterval[1] = max(newInterval[1], interval[1])
+
+        result.append(newInterval)
+
+        return result
